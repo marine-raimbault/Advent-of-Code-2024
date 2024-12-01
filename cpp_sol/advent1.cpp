271f0a1bf72965ed8c3f515ddf38ece2f604a32d
marine-raimbault <marine.raimbault@rwth-aachen.de>
2024-12-01 08:30:29 +0100

    adds advent1 sol2 in cpp

--- cpp_sol/advent1.cpp
+++ cpp_sol/advent1.cpp
@@ -9,6 +9,7 @@ using namespace std;
 
     int main() {
         // Step 1: Read input from a file
+        // std::ifstream file("input1.txt"); // test input file
         std::ifstream file("realInput1.txt");
         if (!file) {
             std::cerr << "Error: Cannot open input file!" << std::endl;
@@ -30,13 +31,31 @@ using namespace std;
         std::sort(rightList.begin(), rightList.end());
 
         // Step 3: Calculate the total distance
-        int totalDistance = 0;
+        int totalDistance{0};
         for (size_t i = 0; i < leftList.size(); ++i) {
             totalDistance += std::abs(leftList[i] - rightList[i]);
         }
 
         // Step 4: Output the result
         std::cout << "Total Distance: " << totalDistance << std::endl;
+
+        // Advent 1 question 2
+        
+        // Step 1: Calculate the frequency of each number in the right list
+        std::vector<int> rightFreq(100000,0);
+        for (int i : rightList) {
+            ++rightFreq[i];
+        }
+        
+        int simScore{0};
+
+        // Step 2: Calculate the similarity score
+        for(int i : leftList){
+            simScore += i * rightFreq[i];
+        }
+
+        // Step 3: Output the result
+        std::cout << "Similarity Score: " << simScore << std::endl;
 
         return 0;
     }
